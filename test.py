53c62d5e524717874591de600ecd94c27773ce2f
Shreesh Swaraj <shreesh.swaraj300@gmail.com>
2020-08-06 21:28:26 +0530

    Added Residual Network implementation of UNet

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ import cv2
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Concatenate, Input
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Concatenate
+from keras.layers import Input, BatchNormalization, Activation, Add, Layer, Flatten
 from keras.models import Model
 
 seed = 2020
@@ -88,6 +89,77 @@ def UNet():
     model = Model(inputs, outputs)
     return model
 
+def bn_act(x, act=True):
+    x = BatchNormalization()(x)
+    if act == True:
+        x = Activation("relu")(x)
+    return x
+
+def conv_block(x, filters, kernel_size=(3,3), padding="same", strides=1):
+    conv = bn_act(x)
+    conv = Conv2D(filters, kernel_size, padding=padding, strides=strides)(conv)
+    return conv
+
+def stem(x, filters, kernel_size=(3,3), padding="same", strides=1):
+    conv = Conv2D(filters, kernel_size, padding=padding, strides=strides)(x)
+    conv = conv_block(conv, filters, kernel_size=kernel_size, padding=padding, strides=strides)
+    
+    shortcut = Conv2D(filters, kernel_size=(1,1), padding=padding, strides=strides)(x)
+    shortcut = bn_act(shortcut, act=False)
+    
+    output = Add()([conv, shortcut])
+    return output
+
+def residual_block(x, filters, kernel_size=(3,3), padding="same", strides=1):
+    res = conv_block(x, filters, kernel_size=kernel_size, padding=padding, strides=strides)
+    res = conv_block(res, filters, kernel_size=kernel_size, padding=padding, strides=1)    
+    
+    shortcut = Conv2D(filters, kernel_size=(1,1), padding=padding, strides=strides)(x)
+    shortcut = bn_act(shortcut, act=False)
+    
+    output = Add()([res, shortcut])
+    return output
+
+def upsample_concat_block(x, xskip):
+    u = UpSampling2D((2, 2))(x)
+    concat = Concatenate()([u ,xskip])
+    return concat
+
+#### ResUnet ###
+def ResUNet():
+    f = [16, 32, 64, 128, 256]
+    inputs = Input((IMAGE_SIZE, IMAGE_SIZE, 3))
+    
+    # ENCODER
+    e0 = inputs
+    e1 = stem(e0, f[0])
+    e2 = residual_block(e1, f[1], strides=2)
+    e3 = residual_block(e2, f[2], strides=2)
+    e4 = residual_block(e3, f[3], strides=2)
+    e5 = residual_block(e4, f[4], strides=2)
+    
+    # BRIDGE
+    b0 = conv_block(e5, f[4], strides=1)
+    b1 = conv_block(b0, f[4], strides=1)
+    
+    # DECODER
+    u1 = upsample_concat_block(b1, e4)
+    d1 = residual_block(u1, f[4])    
+ 
+    u2 = upsample_concat_block(d1, e3)
+    d2 = residual_block(u2, f[3])
+    
+    u3 = upsample_concat_block(d2, e2)
+    d3 = residual_block(u3, f[2])
+    
+    u4 = upsample_concat_block(d3, e1)
+    d4 = residual_block(u4, f[1])
+    
+    outputs = Conv2D(1, (1,1), padding="same", activation="sigmoid")(d4)
+    model = Model(inputs, outputs)
+    return model
+
+
 IMAGE_SIZE = 128
 TEST_PATH = "dataset/stage1_test/"
 BATCH_SIZE = 65
@@ -99,24 +171,32 @@ x= gen.__getitem__(0)
 print(x.shape)
 
 
-model = UNet()
-model.load_weights('UNetW.h5')
+model1 = UNet()
+model2 = ResUNet()
 
-result = model.predict(x)
-result = result > 0.5
+model2.load_weights('ResUNet.h5')
+model1.load_weights('UNetW.h5')
+
+result1 = model1.predict(x)
+result1 = result1 > 0.5
+
+result2 = model2.predict(x)
+result2 = result2 > 0.5
+
+
 
 fig = plt.figure()
 fig.subplots_adjust(hspace=0.4, wspace=0.4)
-ax = fig.add_subplot(1,2,1)
-ax.imshow(x[50], cmap="gray")
-ax = fig.add_subplot(1,2,2)
-ax.imshow(np.reshape(result[50]*255, (IMAGE_SIZE, IMAGE_SIZE)), cmap="gray")
+ax = fig.add_subplot(1,3,1)
+ax.imshow(x[25], cmap="gray")
+plt.title("Original Image")
+ax = fig.add_subplot(1,3,2)
+ax.imshow(np.reshape(result1[25]*255, (IMAGE_SIZE, IMAGE_SIZE)), cmap="gray")
+plt.title("UNet Mask")
+ax = fig.add_subplot(1,3,3)
+ax.imshow(np.reshape(result2[25]*255, (IMAGE_SIZE, IMAGE_SIZE)), cmap="gray")
+plt.title("ResUNet Mask")
 plt.show()
-
-
-
-
-
 
 
 
